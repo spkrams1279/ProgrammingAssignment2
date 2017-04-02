4a37c92b0aa564150b406377987b6d2ef4c62856
spkrams1279 <spkrams1279@gmail.com>
2017-04-02 15:45:31 +0530

    Week3: Programming Assignment - Cache Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Week3: Programming assignment to create functions that will create matrix and return the cached inverted matrix
 
-## Write a short comment describing this function
+
+## Below function will return a list that will help user to get/set matrix and get/set inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invertedMatrix <- NULL
+        set <- function(y){
+                x <- y
+                invertedMatrix <<- NULL
+        }
+        get <- function(){
+                x
+        }
+        setInvertedMatrix <- function(inverted){
+                invertedMatrix <<- inverted
+        }
+        getInvertedMatrix <- function(){
+                invertedMatrix
+        }
+        list(get = get, set = set, getInvertedMatrix = getInvertedMatrix, setInvertedMatrix = setInvertedMatrix)
 }
 
 
-## Write a short comment describing this function
+## Below function will get a matrix created using the above function, invert that matrix and store it. 
+## this functon will try to retrieve the inverted matrix from cache first before calling the solve function to invert
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverted <- x$getInvertedMatrix()
+        if(!is.null(inverted)){
+                message("getting from cached data")
+                return(inverted)
+        }
+        inverted <- solve(x$get(), ...)
+        x$setInvertedMatrix(inverted)
+        inverted
 }
